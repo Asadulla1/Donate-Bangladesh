f48ee59c2a08695454b3d135316e368d8ad070cd
Asadulla1 <asadullaalmamuncse@gmail.com>
2024-09-23 01:47:12 +0600

    design Done

---
+++ JS/menu.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburg-menu").addEventListener("click", () => {
+  const menuContent = document.getElementById("hamburg-menu-content");
+  menuContent.classList.toggle("hidden");
+});
